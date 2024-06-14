f8a273c326167463ce403bc5c7187bbfda2603c5
fahad-githubaction <evanhaque1@gmail.com>
2024-06-13 23:57:21 -0400

    attempted chat bot data fetching

--- app/chatbot/page.tsx
+++ app/chatbot/page.tsx
@@ -6,103 +6,47 @@ import { Input } from "@/components/ui/input"
 
 
 const Chatbot2 = () => {
-    const [chats, setChats] = useState<any>([]);
-    const [current_id, setCurrentId] = useState("");
-    const [message, setMessage] = useState<string>("");
-    const supabase = createClient()
+    const [chats, setChats] = useState<any>([])
+    const [id, setId] = useState<string>("")
     const messagesEndRef = useRef<HTMLDivElement | null>(null);
+    const supabase = createClient()
+
+    useEffect(() => {
+        const getInfo = async () => {
+            const {data: {user}} = await supabase.auth.getUser()
+            const userId = user?.id;
+            if (userId) setId(userId)
+
+            try {
+            const {data , error} = await supabase.from("chatbot").select("messages").eq("id", userId)
+            if (data) console.log(data[0])
+            console.log(userId)
+            } catch (error) {
+                console.error('Error fetching messages:', error);
+                return;
+            }
+
+            //const message = messages?.map(message => messages.messages);
+            //if (message) {
+            //    const confirmedMessage = messages[0]
+            //    setChats(confirmedMessage);
+            //    console.log(messages)
+            //}
+            //else {
+            //    console.error('Error fetching messages:', error);
+            //    return;
+            //}
+        }
+        getInfo()
+        }, [])
+
 
     const scrollToBottom = () => {
         messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
     }
-
-    useEffect(() => {
-        const getInfo = async () => {
-            const {data: {user}} = await supabase.auth.getUser();
-            const id = user?.id;
-            if (id) {
-                setCurrentId(id);
-                console.log(id)
-                return id
-            }
-            else {
-                console.error('Error fetching user:')
-                return;
-            }
-        }
-        const getChat = async () => {
-            const id = await getInfo()
-            if (id) {
-                const { data, error } = await supabase
-                    .from('chatbot')
-                    .select('messages')
-                    .eq('id', id);
-                console.log(data, error)
-
-                //const messages = chats?.map(chat => chat.messages);
-                //if (messages) {
-                //    const message = messages[0]
-                //    setChats(message);
-                //}
-                //else {
-                //    console.error('Error fetching messages:', error);
-                //    return;
-                //}
-            }
-        }
-        getChat()
-    }, [])
-
     useEffect(() => {
         scrollToBottom()
-    }, [chats])
-
-    useEffect(() => {        
-        const channel = supabase
-                .channel('table-db-changes')
-                .on(
-                    'postgres_changes',
-                    {
-                        event: '*',
-                        schema: 'public',
-                        table: 'chats',
-                    },
-                    (payload) => {
-                        const newMessages = (payload.new as { messages: any }).messages;
-                        setChats(newMessages);
-                    }
-                )
-                .subscribe()
-
-        return () => {
-            channel.unsubscribe()
-        }
     }, [])
-    
-    const sendMessage = async (event: React.MouseEvent) => {
-        event.preventDefault();
-    
-        const chatMessage = {
-            sender: current_id,
-            message: message,
-            timestamp: new Date().toISOString()
-        }
-    
-        let latestMessage = chats ? [...chats.flat(), chatMessage] : [chatMessage];
-    
-        console.log(latestMessage)
-        console.log(chats, chatMessage)
-        const { data: newMessage, error } = await supabase
-            .from('chats')
-            .update({ messages: latestMessage})
-            .eq('id', current_id);
-    
-        if (error) {
-            console.error('Error sending message:', error);
-        } else {
-            setMessage('');
-        }
-    }
 
     
     return (
@@ -113,7 +57,7 @@ const Chatbot2 = () => {
                     <p className="m-auto my-4 text-white">Chat With Teyvat's Tinker</p>
                     <div className="mx-4 flex flex-grow flex-col bg-white overflow-auto">
                     {chats?.map((message : any , index : any) => (
-                        message.sender === current_id ? (
+                        message.sender === id? (
                             <p className=" text-end mx-4 text-black my-2" key={index}>{message.message}</p>
                         ) : (
                             <>
@@ -124,10 +68,11 @@ const Chatbot2 = () => {
                     ))}
                     <div ref={messagesEndRef} />
                     </div>
-                    <div className="flex my-4 flex-row m-auto w-full">
-                        <Input className="w-full mx-2" value={message} onChange={(e) => setMessage(e.target.value)} />
-                        <Button  className = "mx-2" onClick={(event) => sendMessage(event)}>Send</Button>
-                    </div>
+                    {//<div className="flex my-4 flex-row m-auto w-full">
+                     //   <Input className="w-full mx-2" value={message} onChange={(e) => setMessage(e.target.value)} />
+                     //   <Button  className = "mx-2" onClick={(event) => sendMessage(event)}>Send</Button>
+                    //</div>
+}
                 </div>
             </>
         )    : (
