6ce399aadb4812fadbf72464f969f0f9a4ea2179
fahad-githubaction <evanhaque1@gmail.com>
2024-06-18 12:47:00 -0400

    made an update server function

--- server/server.py
+++ server/server.py
@@ -1,10 +1,23 @@
 from flask import Flask, request, jsonify
 
+import os
+from supabase import create_client, Client
 
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
+
+ 
 app = Flask(__name__)  
+
+@app.route('/api/<prompt>', methods=['POST'])
+def api(prompt):
+    if request.method == 'POST':
+        data = request.get_json()
+        response = supabase.from_("chatbot").update([{"messages": prompt, "message": data['message']}])
+        return jsonify(response)
 
 
 if (__name__ == "__main__"):
     app.run(port=5000, debug=True)
 
-    
