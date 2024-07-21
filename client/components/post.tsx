aaa84a0e1fdd40ef08dfa7c884e85d7bec22894d
fahad-githubaction <evanhaque1@gmail.com>
2024-07-20 23:58:12 -0400

    added like count

--- client/components/post.tsx
+++ client/components/post.tsx
@@ -9,6 +9,7 @@ const Post = async ({id} : {id: string}) => {
         if (error) {
             throw error;
         }
+        console.log(data)
         return data
     }
     const post = await getPost();
@@ -30,8 +31,16 @@ const Post = async ({id} : {id: string}) => {
     return (
         <div className=" flex flex-col border m-auto border-gray-300 w-[80%] h-[50%]">
             <div className="flex flex-row ml-3 mt-3 items-center space-x-4">
-                <img className="rounded-full w-10" src={user[0].pfp} alt={user[0].username} />
-                <h3 className="text-lg font-semibold">{user[0] && user[0]?.username}</h3>
+            <div className="flex flex-row justify-between ml-3 mt-3 items-center w-full">
+                <div className="flex flex-row items-center space-x-4">
+                    <img className="rounded-full w-10" src={user[0].pfp} alt={user[0].username} />
+                    <h3 className="text-lg font-semibold">{user[0] && user[0]?.username}</h3>
+                </div>
+                <div className="flex space-x-2">
+                    <i className='bx bxs-like'></i>
+                    <p className="text-sm text-gray-500">{post[0].likes.length}</p>
+                </div>
+            </div>
             </div>
             <div className="flex flex-col ml-3 mt-2 ">
                 <h3 className="text-lg font-semibold flex justify-start">{post[0].title}</h3>
